71bc8bbaafa04fa4975c340274032bb2389e3ce4
gzstyp <444141300@qq.com>
2021-02-16 00:29:24 +0800

    限流规则-令牌桶中的url限流,当然可以弄参数限流,比如某个id只能请求一次;IP限流

---
+++ gateway-server/src/main/java/com/example/filter/KeyResolverConfig.java
@@ -0,0 +1,31 @@
+package com.example.filter;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * 限流规则-令牌桶中的url限流,当然可以弄参数限流,比如某个id只能请求一次;IP限流
+ * @作者 田应平
+ * @版本 v1.0
+ * @创建时间 2021-02-15 23:00
+ * @QQ号码 444141300
+ * @Email service@dwlai.com
+ * @官网 http://www.fwtai.com
+*/
+@Configuration
+public class KeyResolverConfig{
+
+    @Bean
+    public KeyResolver pathKeyResolver(){
+        /*return new KeyResolver(){
+            @Override
+            public Mono<String> resolve(final ServerWebExchange exchange){
+                return Mono.just(String.valueOf(exchange.getRequest().getPath()));
+            }
+        };*/
+        System.out.println("有执行吗?");
+        return exchange -> Mono.just(exchange.getRequest().getURI().getPath());//简写
+    }
+}
