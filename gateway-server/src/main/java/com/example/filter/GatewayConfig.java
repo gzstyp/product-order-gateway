0b931dfd60705655fe63eeceedb5b9cd213a8edd
gzstyp <444141300@qq.com>
2021-02-15 14:42:41 +0800

    配置网关路由,配置多个,当然仅配一个也是ok

--- gateway-server/src/main/java/com/example/filter/GatewayConfig.java
+++ gateway-server/src/main/java/com/example/filter/GatewayConfig.java
@@ -5,16 +5,16 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//配置网关路由,配置多个
+//配置网关路由,配置多个,当然仅配一个也是ok
 @Configuration
 public class GatewayConfig{
 
     @Bean // 通过访问gateway http://127.0.0.1:9000/product/3 http://127.0.0.1:9000/order/3
     public RouteLocator routeLocator(final RouteLocatorBuilder builder){
         return builder.routes().route(route ->
-            route.path("/product/**").uri("lb://product-service").filters(new CustomGateWayFilter()).id("product-service")
+            route.path("/product/**").uri("lb://product-service").filters(new ProductGateWayFilter()).id("product-service")
         ).route(route ->
-            route.path("/order/**").uri("lb://order-service").filters(new CustomGateWayFilter()).id("order-service")
+            route.path("/order/**").uri("lb://order-service").filters(new OrderGateWayFilter()).id("order-service")
         ).build();
     }
 }
--- gateway-server/src/main/java/com/example/filter/ProductGateWayFilter.java
+++ gateway-server/src/main/java/com/example/filter/ProductGateWayFilter.java
@@ -7,12 +7,12 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 //自定义网关过滤器(不是全局过滤器)
-public class CustomGateWayFilter implements GatewayFilter, Ordered{
+public class ProductGateWayFilter implements GatewayFilter, Ordered{
 
     // 通过访问gateway测试 http://127.0.0.1:9000/product/2
     @Override
     public Mono<Void> filter(final ServerWebExchange exchange,final GatewayFilterChain chain){
-        System.out.println("自定义网关过滤器(非全局过滤器)已执行");
+        System.out.println("ProductGateWayFilter,自定义网关过滤器(非全局过滤器)已执行");
         return chain.filter(exchange);//继续执行下一个过滤器
     }
 
