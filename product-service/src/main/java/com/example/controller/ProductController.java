cd503fb78c3f1d9c598f445462d12e4cd613a287
gzstyp <444141300@qq.com>
2021-02-15 10:51:02 +0800

    获取请求参数

--- product-service/src/main/java/com/example/controller/ProductController.java
+++ product-service/src/main/java/com/example/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -16,8 +17,15 @@ public class ProductController{
     @Resource
     private ProductService productService;
 
+    // http://127.0.0.1:7070/product/21021509
     @GetMapping("/{id}")
     public Product selectProductById(@PathVariable("id") final Integer id){
+        return productService.selectProductById(id);
+    }
+
+    // http://127.0.0.1:7070/product/info?id=21021509
+    @GetMapping("/info")
+    public Product info(@RequestParam("id") final Integer id){
         return productService.selectProductById(id);
     }
 }
